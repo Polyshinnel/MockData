049af9e3aadde08d3b25c1c1e70ce973855a4815
Andrey Nesterov <polyshinnel@gmail.com>
2025-10-20 16:54:19 +0300

    add BillSupplyService

--- app/Service/BillSupplyService.php
+++ app/Service/BillSupplyService.php
@@ -3,6 +3,10 @@
 namespace App\Service;
 
 use App\Models\Products;
+use App\Models\SupplyBill;
+use App\Models\SupplyBox;
+use App\Models\SupplyProducts;
+use Illuminate\Support\Collection;
 
 class BillSupplyService
 {
@@ -38,17 +42,60 @@ class BillSupplyService
             return $billProcessingData;
         }
 
-        foreach ($billProducts as $billProduct) {
+        $supplyBill = $this->createBill($billData);
 
+        foreach ($billProducts as $billProduct) {
+            $supplyProducts = $this->getFilteredSupplyProducts($billProduct, $billData['supply_id']);
+            if(!$supplyProducts) {
+                $supplyBox = $this->getSupplyBox($billData['supply_id']);
+                $this->createBillProduct($billProduct, $supplyBill, $supplyBox);
+                continue;
+            }
+            if($supplyProducts->count() > 1) {
+                foreach ($supplyProducts as $supplyProduct) {
+
+                }
+            }
         }
     }
 
-    private function getFilteredSupplyProducts($billProduct, $supplyId)
+    private function getFilteredSupplyProducts($billProduct, $supplyId): ?Collection
     {
         $filter = [
             'product_id' => $billProduct['product_id'],
             'supply_id' => $supplyId
         ];
         return Products::where($filter)->get();
+    }
+
+    private function getSupplyBox($supplyId): SupplyBox
+    {
+        return SupplyBox::where('supply_id', $supplyId)->first();
+    }
+
+    private function createBill($billData)
+    {
+        $createArr = [
+            'name' => $billData['name'],
+            'invoice_number' => $billData['invoice_number'],
+            'supply_id' => $billData['supply_id'],
+        ];
+        return SupplyBill::create($createArr);
+    }
+    private function createBillProduct(array $billProduct, SupplyBill $supplyBill, SupplyBox $supplyBox): SupplyProducts
+    {
+        $createArr = [
+            'supply_bill_id' => $supplyBill->id,
+            'supply_box_id' => $supplyBox->id,
+            'supply_request_id' => null,
+            'plan_quantity' => 0,
+            'plan_price' => 0,
+            'fact_quantity' => $billProduct['quantity'],
+            'fact_price' => $billProduct['price'],
+            'weight' => $billProduct['weight'],
+            'dimensions' => $billProduct['dimensions'],
+            'product_id' => $billProduct['product_id'],
+        ];
+        return SupplyProducts::create($createArr);
     }
 }
